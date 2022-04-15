40041f3cbcf7c6dc2355f566df45931db119532b
William Jaimes <william.jaimesfontalvo@gmail.com>
2022-04-14 22:34:36 -0500

    Modifiqué comentarios y agregué otros más. Modifiqué los eventos de búsqueda para que el valor por defecto sea español. Agregué una función y evento que agregan al localStorage un historial con las búsquedas realizadas en formatoJSON

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@
 *********************** */
 
 //VARIABLES
-const LISTA_POKEMONES = []; //Lista que contendrá los objetos/pokemones
-const LISTA_POKEMONES_ENG = [];
+const LISTA_POKEMONES = []; //Array que contendrá los objetos/pokemones
+const LISTA_POKEMONES_ENG = [];//Array que contendrá los objetos/pokemones en ingles
+const HISTORIAL = [];//Array que contendrá el historial de búsquedas
 let entrada, objeto; //Variables a utilizar en la búsqueda
 
 //DOM
@@ -31,6 +32,13 @@ class pokemon { //Clase para el objeto Pokémon
     }
 }
 
+class busquedasUsuario {
+    constructor(id, busca) {
+        this.id = id;
+        this.busqueda = busca;
+    }
+}
+
 //Se instancian los objetos y se agregan al arreglo
 
 LISTA_POKEMONES.push( new pokemon("Bulbasaur", "001", ["Planta", " Veneno"], "Sí", "Primera", "./images/pokemones/001.png"));
@@ -54,130 +62,7 @@ LISTA_POKEMONES.push( new pokemon("Pidgeotto", "017", ["Normal", " Volador"], "S
 LISTA_POKEMONES.push( new pokemon("Rattata", "019", "Normal", "Sí", "Primera", "./images/pokemones/019.png"));
 LISTA_POKEMONES.push( new pokemon("Raticate", "020", "Normal", "No", "Primera", "./images/pokemones/020.png"));
 
-//EVENTOS
-window.onload = cargaPag;
-idioma.onchange = idiomaPag;//Cambiará el idioma de la pagina de acuerdo al interés del usuario
-if (localStorage.getItem("idioma") == "ingles") {
-    boton.onclick = verifyEntry;
-} else if (localStorage.getItem("idioma") == "español"){
-    boton.onclick = verificarEntrada;
-}
-//boton.onclick = verificarEntrada;//activará la búsqueda del Pokémon cuando el usuario haga click en el botón BUSCAR
-entradaUsuario.addEventListener("keypress", function(key) {//activará la búsqueda cuando el usuario presione ENTER
-    if (key.keyCode === 13) {
-        key.preventDefault();
-        verificarEntrada();
-    }
-})
-
-//FUNCIONES
-function verificarEntrada() { //Función que verifica los datos que ingrese el usuario
-
-        entrada = LISTA_POKEMONES.find((el) => el.nombre.toUpperCase() === entradaUsuario.value.toUpperCase());
-        if(entrada != undefined) {
-            const resultado = LISTA_POKEMONES.filter((el) => el.nombre.toUpperCase().includes(entradaUsuario.value.toUpperCase()));
-    
-            //Se asigna a una variable el elemento en la posición cero del array devuelto por filter
-            objeto = resultado[0];
-    
-            //Se agregan los valores de las propiedades del objeto a cada <p> del HTML
-            nombreSalida.innerHTML = objeto.nombre;
-            idSalida.innerHTML = objeto.id;
-            tipoSalida.innerHTML = objeto.tipo;
-            evolucionSalida.innerHTML = objeto.evolucion;
-            generacionSalida.innerHTML = objeto.generacion;
-    
-            //Se modifica el atributo src al elemento img con la imagen del Pokémon buscado
-            document.getElementById("imagen").src = objeto.imagen;
-            
-            //Se cambia a visible el cuadro con las caracteristicas de cada Pokémon, es decir, los valores del objeto
-            document.getElementById("descripcionEng").style.display = "none";
-
-            descripcion.style.display = "flex";
-    
-            //Se deja en blanco el input para las siguientes búsquedas
-            entradaUsuario.value = "";
-        } else {
-            alert("Dato incorrecto");
-        }
-} 
-
-function verifyEntry() { //Función que verifica los datos que ingrese el usuario
-
-        entrada = LISTA_POKEMONES_ENG.find((el) => el.nombre.toUpperCase() === entradaUsuario.value.toUpperCase());
-        if(entrada != undefined) {
-            const resultado = LISTA_POKEMONES_ENG.filter((el) => el.nombre.toUpperCase().includes(entradaUsuario.value.toUpperCase()));
-    
-            //Se asigna a una variable el elemento en la posición cero del array devuelto por filter
-            objeto = resultado[0];
-    
-            //Se agregan los valores de las propiedades del objeto a cada <p> del HTML
-            document.getElementById("nombrePokemonEng").innerHTML = objeto.nombre;
-            document.getElementById("idPokemonEng").innerHTML = objeto.id;
-            document.getElementById("tipoPokemonEng").innerHTML = objeto.tipo;
-            document.getElementById("evolucionPokemonEng").innerHTML = objeto.evolucion;
-            document.getElementById("generacionPokemonEng").innerHTML = objeto.generacion;
-    
-            //Se modifica el atributo src al elemento img con la imagen del Pokémon buscado
-            document.getElementById("imagenEng").src = objeto.imagen;
-            
-            //Se cambia a visible el cuadro con las caracteristicas de cada Pokémon, es decir, los valores del objeto
-            descripcion.style.display = "none";
-
-            document.getElementById("descripcionEng").style.display = "flex";
-    
-            //Se deja en blanco el input para las siguientes búsquedas
-            entradaUsuario.value = "";
-        } else {
-            alert("Dato incorrecto");
-        }
-} 
-
-
-function cargaPag() {
-    if (localStorage.getItem("idioma") == "ingles") {
-        cambioIdiomaIngles();
-    } else {
-        cambioIdiomaEspañol();
-    }
-}
-
-function idiomaPag() {
-    if (idioma.value == 1) {
-        localStorage.setItem("idioma", "ingles");
-        cambioIdiomaIngles();
-        window.location.reload();
-    } else {
-        localStorage.setItem("idioma", "español");
-        cambioIdiomaEspañol();
-        window.location.reload();
-
-    }
-}
-
-function cambioIdiomaIngles() {
-    document.querySelector(".enunIdioma").innerHTML = "Language";
-    document.getElementById("formLabel").innerHTML = "Which Pokémon do you want to search ?"
-    /* document.getElementById("liID").innerHTML = "ID: " + '<p id="idPokemon"></p>';
-    document.getElementById("liNombre").innerHTML = 'Name: <p id="nombrePokemon"></p>';
-    document.getElementById("liTipo").innerHTML = 'Type: <p id="tipoPokemon"></p>';
-    document.getElementById("liEvol").innerHTML = 'Evolution: <p id="evolucionPokemon"></p>';
-    document.getElementById("liGen").innerHTML = 'Generation: <p id="generacionPokemon"></p>'; */
-    boton.value = "SEARCH";
-}
-
-function cambioIdiomaEspañol() {
-    document.querySelector(".enunIdioma").innerHTML = "Lenguaje";
-    document.getElementById("formLabel").innerHTML = "¿Cuál Pokémon quieres buscar?"
-    /* document.getElementById("liID").innerHTML = 'ID: <p id="idPokemon"></p>';
-    document.getElementById("liNombre").innerHTML = 'Nombre: <p id="nombrePokemon"></p>';
-    document.getElementById("liTipo").innerHTML = 'Tipo: <p id="tipoPokemon"></p>';
-    document.getElementById("liEvol").innerHTML = 'Evolución: <p id="evolucionPokemon"></p>';
-    document.getElementById("liGen").innerHTML = 'Generación: <p id="generacionPokemon"></p>'; */
-    boton.value = "BUSCAR";
-}
-
-
+//Se instancian los objetos en idioma inglés y se agregan al arreglo
 
 LISTA_POKEMONES_ENG.push( new pokemon("Bulbasaur", "001", ["Grass", " Poison"], "Yes", "First", "./images/pokemones/001.png"));
 LISTA_POKEMONES_ENG.push( new pokemon("Ivysaur", "002", ["Grass", " Poison"], "Yes", "First", "./images/pokemones/002.png"));
@@ -199,4 +84,142 @@ LISTA_POKEMONES_ENG.push( new pokemon("Pidgeot", "018", ["Normal", " Flying"], "
 LISTA_POKEMONES_ENG.push( new pokemon("Pidgeotto", "017", ["Normal", " Flying"], "Yes", "First", "./images/pokemones/017.png"));
 LISTA_POKEMONES_ENG.push( new pokemon("Rattata", "019", "Normal", "Yes", "First", "./images/pokemones/019.png"));
 LISTA_POKEMONES_ENG.push( new pokemon("Raticate", "020", "Normal", "No", "First", "./images/pokemones/020.png"));
+
+
+//EVENTOS
+window.onload = cargaPag;//Cuando se cargue la pagina revisa el localStorage para mostrar el contenido en el ultimo idioma elegido
+
+idioma.onchange = idiomaPag;//Cambiará el idioma de la pagina de acuerdo al interés del usuario
+
+if (localStorage.getItem("idioma") == "ingles") {//Mostrará los resultados de la busqueda dependiendo del idioma elegido
+    boton.onclick = verifyEntry;
+} else {
+    boton.onclick = verificarEntrada;
+}
+
+entradaUsuario.addEventListener("keypress", function(key) {//activará la búsqueda cuando el usuario presione ENTER
+    if (key.keyCode === 13) {
+        key.preventDefault();
+        if (localStorage.getItem("idioma") == "ingles") {
+            verifyEntry();
+        } else {
+            verificarEntrada();
+        }
+    }
+})
+
+
+
+//FUNCIONES
+function verificarEntrada() { //Función que verifica los datos que ingrese el usuario mostrando los resultados en español
+
+        entrada = LISTA_POKEMONES.find((el) => el.nombre.toUpperCase() === entradaUsuario.value.toUpperCase());
+        if(entrada != undefined) {
+            const resultado = LISTA_POKEMONES.filter((el) => el.nombre.toUpperCase().includes(entradaUsuario.value.toUpperCase()));
+    
+            //Se asigna a una variable el elemento en la posición cero del array devuelto por filter
+            objeto = resultado[0];
+    
+            //Se agregan los valores de las propiedades del objeto a cada <p> del HTML
+            nombreSalida.innerHTML = objeto.nombre;
+            idSalida.innerHTML = objeto.id;
+            tipoSalida.innerHTML = objeto.tipo;
+            evolucionSalida.innerHTML = objeto.evolucion;
+            generacionSalida.innerHTML = objeto.generacion;
+    
+            //Se modifica el atributo src al elemento img con la imagen del Pokémon buscado
+            document.getElementById("imagen").src = objeto.imagen;
+            
+            //Se cambia a invisible el cuadro con las caracteristicas de cada Pokémon en inglés y a visible en español, es decir, los valores del objeto
+            document.getElementById("descripcionEng").style.display = "none";
+            descripcion.style.display = "flex";
+
+            //Guarda en el localStorage un historial con las búsquedas de los usuarios
+            historial();
+    
+            //Se deja en blanco el input para las siguientes búsquedas
+            entradaUsuario.value = "";
+        } else {
+            alert("Dato incorrecto");
+        }
+} 
+
+function verifyEntry() { //Función que verifica los datos que ingrese el usuario mostrando los resultados en ingles
+
+        entrada = LISTA_POKEMONES_ENG.find((el) => el.nombre.toUpperCase() === entradaUsuario.value.toUpperCase());
+        if(entrada != undefined) {
+            const resultado = LISTA_POKEMONES_ENG.filter((el) => el.nombre.toUpperCase().includes(entradaUsuario.value.toUpperCase()));
+    
+            //Se asigna a una variable el elemento en la posición cero del array devuelto por filter
+            objeto = resultado[0];
+    
+            //Se agregan los valores de las propiedades del objeto a cada <p> del HTML
+            document.getElementById("nombrePokemonEng").innerHTML = objeto.nombre;
+            document.getElementById("idPokemonEng").innerHTML = objeto.id;
+            document.getElementById("tipoPokemonEng").innerHTML = objeto.tipo;
+            document.getElementById("evolucionPokemonEng").innerHTML = objeto.evolucion;
+            document.getElementById("generacionPokemonEng").innerHTML = objeto.generacion;
+    
+            //Se modifica el atributo src al elemento img con la imagen del Pokémon buscado
+            document.getElementById("imagenEng").src = objeto.imagen;
+            
+            //Se cambia a invisible el cuadro con las caracteristicas de cada Pokémon en español y a visible en ingles, es decir, los valores del objeto
+            descripcion.style.display = "none";
+            document.getElementById("descripcionEng").style.display = "flex";
+
+            //Guarda en el localStorage un historial con las búsquedas de los usuarios
+            historial();
+    
+            //Se deja en blanco el input para las siguientes búsquedas
+            entradaUsuario.value = "";
+        } else {
+            alert("Wrong data");
+        }
+} 
+
+
+function cargaPag() {//Función que revisará en el localStorage el ultimo idioma elegido por el usuario para colocarlo automáticamente
+    if (localStorage.getItem("idioma") == "ingles") {
+        cambioIdiomaIngles();
+    } else {
+        cambioIdiomaEspañol();
+    }
+}
+
+function idiomaPag() {//Función que cambia el idioma de la página de acuerdo a la elección del usuario
+    if (idioma.value == 1) {
+        localStorage.setItem("idioma", "ingles");
+        cambioIdiomaIngles();
+        window.location.reload();
+    } else {
+        localStorage.setItem("idioma", "español");
+        cambioIdiomaEspañol();
+        window.location.reload();
+    }
+}
+
+function cambioIdiomaIngles() {//Función que cambia los textos de los elementos a inglés
+    document.querySelector(".enunIdioma").innerHTML = "Language";
+    document.getElementById("formLabel").innerHTML = "Which Pokémon do you want to search ?"
+    boton.value = "SEARCH";
+}
+
+function cambioIdiomaEspañol() {//Función que cambia los textos de los elementos a español
+    document.querySelector(".enunIdioma").innerHTML = "Lenguaje";
+    document.getElementById("formLabel").innerHTML = "¿Cuál Pokémon quieres buscar?"
+    boton.value = "BUSCAR";
+}
+
+function historial() {//Función que agrega al localStorage el array de objetos que contiene en historial de búsquedas
+    let pokemonBuscado = entradaUsuario.value;
+    let clave = HISTORIAL.length;
+
+    //Se instancia el objeto búsquedasUsuario y se agrega al array
+    let entrada = new busquedasUsuario(clave, pokemonBuscado);
+    HISTORIAL.push(entrada)
+
+    //Se almacena en el localStorage el arreglo de objetos convertido a JSON 
+    localStorage.setItem("historial", JSON.stringify(HISTORIAL));
+
+}
 
